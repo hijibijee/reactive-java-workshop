25b20a04f74bbffcc41360e3804fa68b02fd8f5c
hijibijee <tashrifahmedtaif07@gmail.com>
2026-01-27 22:24:41 +0600

    Exercise 4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 
 public class Exercise4 {
 
+    /*
+    FLUX - 0 to N numbers of async events
+    MONO - 0 to 1 number of async event
+     */
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(x -> System.out.println(x));
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        Integer number = ReactiveSources.intNumberMono().block();
+        System.out.println("Variable instantiated by blocking Mono: " + number);
 
         System.out.println("Press a key to end");
         System.in.read();
