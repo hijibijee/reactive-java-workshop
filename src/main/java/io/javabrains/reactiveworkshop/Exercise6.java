56b71998b58f7f6dc5fbfd126e34a11c388e02f5
hijibijee <tashrifahmedtaif07@gmail.com>
2026-01-28 20:31:19 +0600

    Exercise 6

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -1,6 +1,11 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 public class Exercise6 {
 
@@ -10,11 +15,16 @@ public class Exercise6 {
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        // TODO: Write code here
+        String monoValue = ReactiveSources.unresponsiveMono().block(); // It will throw IllegalStateException on timeout
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        List<String> list = ReactiveSources.unresponsiveFlux()
+            .timeout(Duration.ofSeconds(5))
+            .onErrorResume(TimeoutException.class, e -> Flux.empty())
+            .collectList()
+            .block();
+        System.out.println(list);
 
         System.out.println("Press a key to end");
         System.in.read();
