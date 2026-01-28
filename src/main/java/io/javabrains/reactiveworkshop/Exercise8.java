93c1ae1cdfc73b24b81100100b7bbbcec8a07ccb
hijibijee <tashrifahmedtaif07@gmail.com>
2026-01-28 18:34:09 +0600

    Exercise 8

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.SignalType;
 
 import java.io.IOException;
 
@@ -12,14 +13,38 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+
+        /* This will completely swallow the error (no error will pop up in console) */
+        ReactiveSources.intNumbersFluxWithException()
+            .subscribe(x -> System.out.println(x), err -> System.out.println(err.getMessage()));
+
+        /* This will execute the lambda on error, but the exception will still pop up in console */
+        ReactiveSources.intNumbersFluxWithException()
+            .doOnError(err -> System.out.println(err.getMessage()))
+            .subscribe(x -> System.out.println(x));
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        /* lambda will be executed on error, but the subscription will continue to consume next items from the flux */
+        ReactiveSources.intNumbersFluxWithException()
+            .onErrorContinue((err, item) -> System.out.println("Error: " + err.getMessage()))
+            .subscribe(x -> System.out.println(x));
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        /* It will switch to the new flux on error */
+        ReactiveSources.intNumbersFluxWithException()
+            .onErrorResume(err -> Flux.just(-1, -2))
+            .subscribe(x -> System.out.println(x));
+
+        ReactiveSources.intNumbersFluxWithException()
+            .doFinally(signalType -> {
+                if (signalType == SignalType.ON_COMPLETE) {
+                    System.out.println("Job done!");
+                } else if (signalType == SignalType.ON_ERROR) {
+                    System.out.println("Error!");
+                }
+            })
+            .subscribe(x -> System.out.println(x));
 
         System.out.println("Press a key to end");
         System.in.read();
