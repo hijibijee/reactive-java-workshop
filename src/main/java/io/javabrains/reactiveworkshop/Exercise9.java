f53a5b98056ee8382541076601b737fa64906999
hijibijee <tashrifahmedtaif07@gmail.com>
2026-01-28 20:31:31 +0600

    Exercise 9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,24 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+            .count()
+            .subscribe(x -> System.out.println());
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+            .collectList()
+            .subscribe(list -> System.out.println(list));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        /*
+        Input -> 1, 2, 3, 4
+        Output -> 1 + 2, 3 + 4
+        */
+        ReactiveSources.intNumbersFlux()
+            .buffer(2)
+            .map(list -> list.get(0) + list.get(1))
+            .subscribe(x -> System.out.println(x));
 
         System.out.println("Press a key to end");
         System.in.read();
